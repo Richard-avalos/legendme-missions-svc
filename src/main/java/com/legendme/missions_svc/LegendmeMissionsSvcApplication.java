d857482a3f5823539a102c58d4df1526996032c2
DESTRUERE\richa <avalosrichard041@gmail.com>
2025-10-15 11:27:46 -0600

    Estructura inicial (Configuracion de JWT)

---
+++ src/main/java/com/legendme/missions_svc/LegendmeMissionsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.legendme.missions_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendmeMissionsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendmeMissionsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legendme/missions_svc/MissionsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legendme.missions_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
