738ccabd24d9513adddb79b0a060a74c614983e5
Rael <rodriguezreneisrael@gmail.com>
2025-10-21 20:12:05 -0600

    -Se crearon la logica base de mission y categoria(Domain,Entity,Adapters,PersitenceAdapter,etc -Se configuraron los endpoints y la logica correspondiente -Se instalaron la logica JWT al proyecto para la seguridad e integridad de datos -Entre otras cosas -Manejo de Logs y Observaciones

--- src/main/java/com/legendme/missions_svc/util/JwtUtils.java
+++ src/main/java/com/legendme/missions_svc/util/JwtUtils.java
@@ -6,16 +6,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
-
+/**
+ * Utilidad para manejar JSON Web Tokens (JWT).
+ */
 @Component
 public class JwtUtils {
 
+    /** Secreto utilizado para firmar y validar los tokens JWT */
     private final String secret;
 
+    /**
+     * Constructor que inyecta el secreto JWT desde las propiedades de la aplicación.
+     *
+     * @param secret el secreto utilizado para firmar y validar los tokens JWT
+     */
     public JwtUtils(@Value("${jwt.secret}") String secret) {
         this.secret = secret;
     }
-
+    /**
+     * Valida el token JWT y extrae el sujeto (subject).
+     *
+     * @param token el token JWT a validar
+     * @return el sujeto extraído del token
+     */
     public String validateTokenAndGetSubject(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(secret.getBytes())
@@ -25,6 +38,12 @@ public class JwtUtils {
         return claims.getSubject();
     }
 
+    /**
+     * Extrae el ID de usuario del encabezado de autorización.
+     *
+     * @param authHeader el encabezado de autorización que contiene el token JWT
+     * @return el ID de usuario extraído del token
+     */
     public UUID extractUserIdFromAuthorizationHeader(String authHeader) {
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             throw new RuntimeException("Authorization header missing or invalid");
